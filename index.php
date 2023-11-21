8f3e59e239f43ec135a1e7f9100888a0838017b8
AxelParedes <124729598+AxelParedes@users.noreply.github.com>
2023-11-20 19:04:33 -0600

    Update index.php
    Pagina de inicio

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Lista de Tareas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="app">
+        <h1>Lista de Tareas</h1>
+        <form id="taskForm">
+            <input type="text" id="taskInput" placeholder="Nueva tarea">
+            <button type="submit">Agregar Tarea</button>
+        </form>
+        <div id="tasksList"></div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
+
 
