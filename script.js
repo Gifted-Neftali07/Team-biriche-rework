8031130fa4a4cbbc6105e1f3d366888892cb61af
AxelParedes <al228506@edu.uaa.mx>
2023-11-20 19:24:15 -0600

    chavales chavales

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function sendTaskToServer(task) {
+    fetch('server.php', {
+        method: 'POST',
+        body: JSON.stringify({ task: task }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.text())
+    .then(data => console.log(data));
+}
+
+document.getElementById('taskForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let task = document.getElementById('taskInput').value;
+    if (task) {
+        addTaskToDOM(task);
+        sendTaskToServer(task);
+        document.getElementById('taskInput').value = '';
+    }
+});
 
--- server.php
+++ server.php
@@ -1,1 +1,20 @@
+<?php
+// Por simplificación, las tareas se almacenan en un array
+// En una implementación real, aquí se conectaría con una base de datos
+
+$tasks = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $task = $_POST['task'] ?? '';
+    if ($task) {
+        $tasks[] = $task;
+        echo "Tarea agregada";
+    }
+} else {
+    foreach ($tasks as $task) {
+        echo $task . "<br>";
+    }
+}
+?>
+
 
